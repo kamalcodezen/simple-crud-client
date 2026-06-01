2fd3dda2fc7405fbbf20951d4fa91216f21ca3f9
kamalcodezen <kamaluddin7908@gmail.com>
2026-06-01 14:18:58 +0530

    modal added

---
+++ src/app/components/AddUserModal.jsx
@@ -0,0 +1,65 @@
+"use client";
+import { Envelope } from "@gravity-ui/icons";
+import { Button, Input, Label, Modal, Surface, TextField } from "@heroui/react";
+
+const AddUserModal = () => {
+  return (
+    <Modal>
+      <Button variant="secondary">Add User</Button>
+      <Modal.Backdrop>
+        <Modal.Container placement="auto">
+          <Modal.Dialog className="sm:max-w-md">
+            <Modal.CloseTrigger />
+            <Modal.Header>
+              <Modal.Heading>Add User</Modal.Heading>
+              <p className="mt-1.5 text-sm leading-5 text-muted">
+                Fill out the form below.
+              </p>
+            </Modal.Header>
+            <Modal.Body className="p-6">
+              <Surface variant="default">
+                <form className="flex flex-col gap-4">
+                  <TextField
+                    className="w-full"
+                    name="name"
+                    type="text"
+                    variant="secondary"
+                  >
+                    <Label>Name</Label>
+                    <Input placeholder="Enter your name" />
+                  </TextField>
+                  <TextField
+                    className="w-full"
+                    name="email"
+                    type="email"
+                    variant="secondary"
+                  >
+                    <Label>Email</Label>
+                    <Input placeholder="Enter your email" />
+                  </TextField>
+                  <TextField
+                    className="w-full"
+                    name="phone"
+                    type="text"
+                    variant="secondary"
+                  >
+                    <Label>Role</Label>
+                    <Input placeholder="Enter your role" />
+                  </TextField>
+                </form>
+              </Surface>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button slot="close" variant="secondary">
+                Cancel
+              </Button>
+              <Button slot="close">Add</Button>
+            </Modal.Footer>
+          </Modal.Dialog>
+        </Modal.Container>
+      </Modal.Backdrop>
+    </Modal>
+  );
+};
+
+export default AddUserModal;
--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,14 +1,17 @@
 import { getUsersData } from "@/lib/data";
 import UsersTable from "../components/UsersTable";
 import { deleteUserData } from "@/lib/actions";
-
+import AddUserModal from "../components/AddUserModal";
 
 const UsersPage = async () => {
   const users = await getUsersData();
   console.log(users);
   return (
-    <div>
-      <h2>{users.length}</h2>
+    <div className="w-10/12 mx-auto space-y-4 py-5">
+      <div className="flex justify-between items-center">
+        <h2>{users.length}</h2>
+        <AddUserModal />
+      </div>
       <UsersTable users={users} deleteUserAction={deleteUserData} />
     </div>
   );
